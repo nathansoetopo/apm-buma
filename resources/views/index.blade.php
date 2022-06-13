2a36cf481ff3270dec238854da24483458b35e41
nathansoetopo <nathan_aris2121@student.uns.ac.id>
2022-06-13 19:07:25 +0700

    Footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -222,17 +222,20 @@
             <div class="container">
                 <div class="row">
                     <!-- Footer Location-->
-                    <div class="col-lg-4 mb-5 mb-lg-0">
-                        <h4 class="text-uppercase mb-4">Location</h4>
+                    <div class="col-lg-3 mb-5 mb-lg-0">
+                        <div class="row">
+                            <img class="rounded mb-5" src="{{ asset('startboot/logobuma.png') }}" alt="..." />
+                        </div>
+                        {{-- <h4 class="text-uppercase mb-4">Hubungi Kami</h4>
                         <p class="lead mb-0">
                             2215 John Daniel Drive
                             <br />
                             Clark, MO 65243
-                        </p>
+                        </p> --}}
                     </div>
                     <!-- Footer Social Icons-->
-                    <div class="col-lg-4 mb-5 mb-lg-0">
-                        <h4 class="text-uppercase mb-4">Around the Web</h4>
+                    <div class="col-lg-5 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Hubungi Kami</h4>
                         <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
                         <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
                         <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
@@ -240,7 +243,7 @@
                     </div>
                     <!-- Footer About Text-->
                     <div class="col-lg-4">
-                        <h4 class="text-uppercase mb-4">About Freelancer</h4>
+                        <h4 class="text-uppercase mb-4">Tentang Kami</h4>
                         <p class="lead mb-0">
                             Freelance is a free to use, MIT licensed Bootstrap theme created by
                             <a href="http://startbootstrap.com">Start Bootstrap</a>
