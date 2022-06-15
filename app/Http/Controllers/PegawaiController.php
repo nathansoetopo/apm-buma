5e0a3d56b1f175c4e2940e195ce9ce9344f0692e
nathansoetopo <nathan_aris2121@student.uns.ac.id>
2022-06-15 15:19:08 +0700

    Index

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -4,17 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
 class PegawaiController extends Controller
 {
     public function index()
     {
-        return view('index');
+        //return view('index');
+        //$user = User::find(Auth::user()->id)->orderBy('created_at')->first();
+        $last = DB::table('quiz_users')->where('user_id', Auth::user()->id)->latest('id')->first();
+        return view('index', ['data' => $last]);
     }
 
     public function riwayatTest(){
-        $user = User::find(Auth::user()->id)->quiz_grade()->get();
+        $user = User::find(Auth::user()->id)->quiz_grade()->paginate(5);
         return view("riwayat", ["user"=>$user]);
     }
 }
