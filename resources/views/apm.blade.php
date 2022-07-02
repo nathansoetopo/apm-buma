386f78903599e37a8d20d4e179495dc769a8d48b
nathansoetopo <nathan_aris2121@student.uns.ac.id>
2022-07-02 13:44:07 +0700

    Update Revisi

--- resources/views/apm.blade.php
+++ resources/views/apm.blade.php
@@ -12,13 +12,45 @@
             width: 100%;
             background-color: red;
         }
+        .circle2 {
+            width: 150px;
+            height: 150px;
+            border: 1px solid #000;
+            border-radius: 50%;
+        }
 
+        .triangle-up {
+            height: 0;
+            width: 150px;
+            border-bottom: 150px solid;
+            border-left: 100px solid transparent;
+            border-right: 100px solid transparent;
+        }
+
+        .kotak {
+            width: 150px;
+            height: 150px;
+        }
+
+        .rectangle {
+            width: 150px;
+            height: 100px;
+        }
+
+        .oval {
+            width: 160px;
+            height: 80px;
+            background: #a84909;
+            border-radius: 50%;
+        }
     </style>
 </head>
 
 <body>
     <button id="button" onclick="spwanerData()">
-        <h1>Click Me</h1>
+        <center>
+            <div class="bg-success" id="shape"></div>
+        </center>
     </button>
     <script>
         $(document).ready(function () {
@@ -30,7 +62,6 @@
                 method: 'POST',
             })
         });
-
     </script>
     <script>
         showButton();
@@ -51,7 +82,6 @@
                 console.log('Penilaian Jalan');
                 document.getElementById("button").style.visibility = "hidden";
                 timer = Math.floor((Math.random() * 5) + 1);
-                console.log(click);
                 clearInterval(timerVariable)
                 nilai.push(total);
                 total = 0;
@@ -81,7 +111,6 @@
                     setTimeout(myFunction, timer * 1000);
                 } else if (click > 20) {
                     document.getElementById("button").style.visibility = "hidden";
-                    // alert('Selesai APM');
                     window.location = "/";
                 }
             }
@@ -114,8 +143,80 @@
             var color_now = '#000000'.slice(0, -color.length) + color;
             document.getElementById("button").style.visibility = "visible";
             document.getElementById("button").style.background = color_now;
+            lightOrDark(color_now);
         }
 
+        function lightOrDark(color) {
+            var r, g, b, hsp;
+            if (color.match(/^rgb/)) {
+                color = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/);
+
+                r = color[1];
+                g = color[2];
+                b = color[3];
+            } else {
+                color = +("0x" + color.slice(1).replace(
+                    color.length < 5 && /./g, '$&$&'));
+
+                r = color >> 16;
+                g = color >> 8 & 255;
+                b = color & 255;
+            }
+            hsp = Math.sqrt(
+                0.299 * (r * r) +
+                0.587 * (g * g) +
+                0.114 * (b * b)
+            );
+            var shape = document.getElementById("shape");
+            function get_random (list) {
+                var result = list[Math.floor((Math.random()*list.length))];
+                console.log(result);
+                if(result == 'segitiga'){
+                    shape.className = "triangle-up";
+                    if (hsp > 127.5) {
+                        shape.style.borderColor = 'black';
+                    } else {
+                        shape.style.borderColor = 'white';
+                    }
+                }else if(result == 'bundar'){
+                    shape.className = "circle2";
+                    if (hsp > 127.5) {
+                        shape.style.background = 'black';
+                    } else {
+                        shape.style.background = 'white';
+                    }
+                }else if(result == 'kotak'){
+                    shape.className = "kotak";
+                    if (hsp > 127.5) {
+                        shape.style.borderColor = 'black';
+                    } else {
+                        shape.style.borderColor = 'white';
+                    }
+                }else if(result == 'oval'){
+                    shape.className = "oval";
+                    if (hsp > 127.5) {
+                        shape.style.borderColor = 'black';
+                    } else {
+                        shape.style.borderColor = 'white';
+                    }
+                }else if(result == 'rectangle'){
+                    shape.className = "rectangle";
+                    if (hsp > 127.5) {
+                        shape.style.background = 'black';
+                    } else {
+                        shape.style.background = 'white';
+                    }
+                }else if(result == 'kotak'){
+                    shape.className = "kotak";
+                    if (hsp > 127.5) {
+                        shape.style.background = 'black';
+                    } else {
+                        shape.style.background = 'white';
+                    }
+                }
+            }
+            get_random(['segitiga','bundar', 'kotak', 'oval', 'rectangle'])
+        }
     </script>
 </body>
 
